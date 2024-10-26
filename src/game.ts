8d22c61fa5eca447dc751cfcfaeb18d4d9e3b904
bogdan.mantaluta <bogdan.mantaluta@eos-it.ro>
2024-10-26 17:55:25 +0300

    fix for bee image not loading when deployed

--- src/game.ts
+++ src/game.ts
@@ -85,7 +85,7 @@ function createBees(): void {
         const beeCount = Math.floor(Math.random() * (max - min + 1)) + min;
         for (let i = 0; i < beeCount; i++) {
             const beeImg = document.createElement('img');
-            beeImg.src = '/assets/bee.png';
+            beeImg.src = './assets/bee.png';
             beeImg.alt = 'bee';
             beeImg.classList.add('bee');
             const delay = Math.random() * 2;
